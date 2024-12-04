c483748f7eb3858f1a3b4f93cc2810941697568a
handabao <18862963156@163.com>
2024-12-04 11:51:48 +0800

    插入数据

--- src/mysql_create_table/src/main.cpp
+++ src/mysql_create_table/src/main.cpp
@@ -2,6 +2,7 @@
 #include <mysql.h>
 #include <thread>
 #include <format>
+#include <sstream>
 
 int main(int argc, char *argv[])
 {
@@ -27,14 +28,19 @@ int main(int argc, char *argv[])
 
     /// 1 创建表
     const auto tableName = "t_image";
-    const auto sql       = std::format("CREATE TABLE IF NOT EXISTS `{}` (" ///表名
+    const auto cloID     = "id";
+    const auto cloName   = "name";
+    const auto cloPath   = "path";
+    const auto cloSize   = "size";
+    auto       sql       = std::format("CREATE TABLE IF NOT EXISTS `{}` (" ///表名
                                        "`{}` int AUTO_INCREMENT,"          /// 图片id
                                        "`{}` varchar(1024),"               /// 图片名称
                                        "`{}` varchar(2046),"               /// 图片路径
                                        "`{}` int,"                         /// 图片大小
                                        " PRIMARY KEY(`{}`));",
-                                       tableName, "id", "name", "path", "size", "id");
-    int        re        = mysql_query(&mysql, sql.c_str());
+                                       tableName, cloID, cloName, cloPath, cloSize, cloID);
+    // std::cout << sql << std::endl;
+    int re = mysql_query(&mysql, sql.c_str());
     if (re != 0)
     {
         std::cout << "create table " << tableName << "failed!!!: " << std::endl;
@@ -45,6 +51,23 @@ int main(int argc, char *argv[])
     }
 
     /// 2 插入数据 CLIENT_MULTI_STATEMENTS
+    for (int i = 0; i < 1000; i++)
+    {
+        sql = std::format("INSERT INTO {} (`{}`, `{}`, `{}`) VALUES ('{}', '{}', '{}');", tableName, cloName, cloPath,
+                          cloSize, std::format("image_{}.jpg", i), R"(D:\images)", 10240);
+
+        // std::cout << sql << std::endl;
+        re = mysql_query(&mysql, sql.c_str());
+        if (re == 0)
+        {
+            int count = mysql_affected_rows(&mysql);
+            std::cout << "mysql_affected_rows " << count << " id =" << mysql_insert_id(&mysql) << std::endl;
+        }
+        else
+        {
+            std::cout << "insert failed!" << mysql_error(&mysql) << std::endl;
+        }
+    }
 
 
     //3 修改数据
